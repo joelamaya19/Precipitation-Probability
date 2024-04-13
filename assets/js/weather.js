e630d5caa9c9c55cb8fb413ecf79c2df2d44da76
joelamaya19 <joelamaya99@gmail.com>
2024-04-13 18:25:55 -0500

    added background image to correspond to weather description, added country name next to cityname

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -64,29 +64,55 @@ function displayCurrentWeather(data) {
     console.log('Todays weather!!!');
 
     const cityName = data.name;
+    const cityCountry = data.sys.country;
     const cityTemp = kelvinToFahrenheit(data.main.temp);
     const cityTempFeels = kelvinToFahrenheit(data.main.feels_like);
     const cityTempMax = kelvinToFahrenheit(data.main.temp_max);
     const cityTempMin = kelvinToFahrenheit(data.main.temp_min);
     const cityHumidity = data.main.humidity;
     const cityWindSpeed = parseInt(data.wind.speed * 2.23694);
-    const cityWeather = capitalizeFirstLetter(data.weather[0].description);
+    const cityWeatherDescription = capitalizeFirstLetter(data.weather[0].description);
+    const mainWeatherDescription = capitalizeFirstLetter(data.weather[0].main);
 
+    // Define a mapping of weather descriptions to background colors
+    const backgroundMap = {
+        'Clear': './assets/images/sunny.gif', // Sunny
+        'Clouds': './assets/images/cloudy2.gif', // Cloudy
+        'Rain': './assets/images/rainy.gif', // Rainy
+        'Thunderstorm': '#575757', // Thunderstorm
+        'Snow': '#F6F6F6', // Snowy
+        // Add more mappings as needed
+    };
+
+    // Get the background color based on the weather description
+    const backgroundImageUrl = backgroundMap[mainWeatherDescription] || '#FFFFFF'; // Default to white if no match found
+
+    const displayWeather = $('#display-weather')
+
+    displayWeather.css({
+        'background-image': 'url("' + backgroundImageUrl + '")',
+        'background-size': '100% auto',
+        'background-position': 'center',
+        'background-repeat': 'no-repeat',
+    });
+    
     // Create a jQuery object for the card
-    let $card = $('<div class="card cityWeather">');
+    let $card = $('<div class="card cityWeather">')
 
     // Create a card body
     let $cardBody = $('<div class="insideCityWeather">');
 
     // Set the card content
-    $cardBody.append('<h5 class="card-title">' + cityName + '</h5>');
-    $cardBody.append('<p class="card-text">Temperature: ' + cityTemp + '°F</p>');
-    $cardBody.append('<p class="card-text">Feels Like: ' + cityTempFeels + '°F</p>');
-    $cardBody.append('<p class="card-text">Max Temperature: ' + cityTempMax + '°F</p>');
-    $cardBody.append('<p class="card-text">Min Temperature: ' + cityTempMin + '°F</p>');
-    $cardBody.append('<p class="card-text">Humidity: ' + cityHumidity + '%</p>');
-    $cardBody.append('<p class="card-text">Wind Speed: ' + cityWindSpeed + ' mph</p>');
-    $cardBody.append('<p class="card-text">Weather: ' + cityWeather + '</p>');
+    $cardBody.append(
+        $('<h5>').addClass('card-title text-center fw-bold fs-14').text(cityName+', ' +cityCountry),
+        $('<p>').addClass('card-text text-left').text('Temperature: ' + cityTemp + '°F'),
+        $('<p>').addClass('card-text').text('Feels Like: ' + cityTempFeels + '°F'),
+        $('<p>').addClass('card-text').text('Max Temperature: ' + cityTempMax + '°F'),
+        $('<p>').addClass('card-text').text('Min Temperature: ' + cityTempMin + '°F'),
+        $('<p>').addClass('card-text').text('Humidity: ' + cityHumidity + '%'),
+        $('<p>').addClass('card-text').text('Wind Speed: ' + cityWindSpeed + ' mph'),
+        $('<p>').addClass('card-text').text('Description: ' + cityWeatherDescription)
+    );
 
     // Append the card body to the card
     $card.append($cardBody);
@@ -109,7 +135,8 @@ function displayForecastWeather(data) {
     // Loop through each forecast data item
     for (let i = 0; i < forecasts.length; i += 8) {
         const forecast = forecasts[i];
-        const date = new Date(forecast.dt_txt);
+        const date = dayjs(forecast.dt_txt);
+        const formattedDate = date.format('dddd - MM/DD/YYYY');
         const temperature = kelvinToFahrenheit(forecast.main.temp);
         const feelsLike = kelvinToFahrenheit(forecast.main.feels_like);
         const tempMin = kelvinToFahrenheit(forecast.main.temp_min);
@@ -118,11 +145,11 @@ function displayForecastWeather(data) {
         const weatherDescription = capitalizeFirstLetter(forecast.weather[0].description);
 
         // Create a card for the forecast
-        const card = $('<div>').addClass('card');
+        const card = $('<div>').addClass('card eachForecast');
         const cardBody = $('<div>').addClass('forecastWeather');
 
         // Populate card body with forecast details
-        const dateElement = $('<p>').text(date.toLocaleDateString());
+        const dateElement = $('<p>').addClass('dateElement text-center fw-bold fs-14').text(formattedDate);
         const temperatureElement = $('<p>').text(`Temperature: ${temperature} °F`);
         const feelsLikeElement = $('<p>').text(`Feels Like: ${feelsLike} °F`);
         const tempMinMaxElement = $('<p>').text(`Min: ${tempMin} °F, Max: ${tempMax} °F`);
